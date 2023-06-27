bbe74cc7458615d54845fbeef6a542583a312510
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-27 21:25:06 +0300

    Create server.rs

---
+++ frontend/src/server.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use warp::Filter;
+use warp::Rejection;
+use serde::Deserialize;
+
+use crate::service;
+
+#[derive(Debug, Deserialize)]
+pub struct FormData {
+    address: String,
+    start_block: String,
+    end_block: String,
+}
+
+pub async fn form_handler(form_data: HashMap<String, String>) -> Result<impl warp::Reply, Rejection> {
+    let address = form_data.get("address").cloned().unwrap_or_default();
+    let start_block = form_data.get("start_block").cloned().unwrap_or_default();
+    let end_block = form_data.get("end_block").cloned().unwrap_or_default();
+
+    let response = service::get_address_info(&address, &start_block, &end_block).await.unwrap_or_default();
+
+    Ok(warp::reply::json(&response))
+}
+
+pub fn submit_form() -> impl Filter<Extract = (HashMap<String, String>,), Error = Rejection> + Clone {
+    warp::path!("submit_form")
+        .and(warp::post())
+        .and(warp::body::form())
+}
+
+pub fn form() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path::end()
+        .and(warp::get())
+        .map(|| "Submit form data here")
+}
