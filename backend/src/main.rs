24ea261f4cfa8d0733e8a0b017e2efed9895d59d
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-27 21:18:36 +0300

    Create main.rs

---
+++ backend/src/main.rs
@@ -0,0 +1,36 @@
+use std::convert::Infallible;
+use reqwest::Client;
+use warp::body::form;
+use warp::Filter;
+use crate::handlers::get_account_data;
+
+mod etherscan;
+mod handlers;
+mod client;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new(); // This should be the initialized reqwest client
+    let client_filter = warp::any().map(move || client.clone());
+
+    let address_info = warp::path!("address")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(client_filter.clone())
+        .and_then(get_account_data);
+
+// Combine all the routes
+    let routes = address_info;
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["POST", "GET", "PUT", "DELETE"])
+        .allow_headers(vec!["Content-Type"]);
+
+    warp::serve(routes.with(cors)).run(([127, 0, 0, 1], 8080)).await;
+}
+
+fn with_client(
+    client: reqwest::Client,
+) -> impl Filter<Extract = (reqwest::Client,), Error = Infallible> + Clone {
+    warp::any().map(move || client.clone())
+}
