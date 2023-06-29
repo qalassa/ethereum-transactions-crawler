c5cc05c452f0d87e2f2f42fa75eb24ebe0ae0179
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-29 13:31:52 +0300

    Update handlers.rs

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -2,6 +2,7 @@ use crate::etherscan::fetch_account_data;
 use warp::{http::StatusCode, Rejection, Reply, reject::custom};
 use reqwest::Client;
 use std::env;
+use chrono::{DateTime, NaiveDate, Utc};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -9,6 +10,7 @@ pub struct AccountDataRequest {
     address: String,
     start_block: String,
     end_block: String,
+    date: String, // Added to handle the new date filter
 }
 
 #[derive(Debug)]
@@ -22,13 +24,21 @@ pub async fn get_account_data(
     body: AccountDataRequest,
     client: Client
 ) -> Result<impl warp::Reply, Rejection> {
-    let AccountDataRequest { address, start_block, end_block } = body;
+    let AccountDataRequest { address, start_block, end_block, date } = body;
+
+    // Parse the date string into a NaiveDate
+    let date = match NaiveDate::parse_from_str(&date, "%Y-%m-%d") {
+        Ok(dt) => dt,
+        Err(e) => {
+            return Err(warp::reject::custom(CustomError { message: "Invalid date format" }));
+        }
+    };
 
     let account_data = fetch_account_data(&client, &address, &start_block, &end_block).await;
 
     match account_data {
-        Ok((eth_balance, token_transfers)) => {
-            let response = warp::reply::json(&{(eth_balance, token_transfers)});
+        Ok((eth_balance, token_transfers, eth_transfers)) => {
+            let response = warp::reply::json(&{ (eth_balance, token_transfers, eth_transfers) });
             Ok(warp::reply::with_status(response, StatusCode::OK))
         },
         Err(_) => Err(warp::reject::custom(CustomError { message: "Failed to fetch account data" })),
