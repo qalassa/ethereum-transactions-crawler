c4d2497b9423f3dea3be31a2cabfd6804e94411d
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-29 13:31:29 +0300

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,14 +10,17 @@ mod client;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let client = Client::new(); // This should be the initialized reqwest client
     let client_filter = warp::any().map(move || client.clone());
 
     let address_info = warp::path!("address")
         .and(warp::post())
+        .and(warp::body::content_length_limit(1024 * 16)) // limiting the content length
         .and(warp::body::json())
         .and(client_filter.clone())
         .and_then(get_account_data);
+
 
 // Combine all the routes
     let routes = address_info;
@@ -27,10 +30,4 @@ async fn main() {
         .allow_headers(vec!["Content-Type"]);
 
     warp::serve(routes.with(cors)).run(([127, 0, 0, 1], 8080)).await;
-}
-
-fn with_client(
-    client: reqwest::Client,
-) -> impl Filter<Extract = (reqwest::Client,), Error = Infallible> + Clone {
-    warp::any().map(move || client.clone())
 }
