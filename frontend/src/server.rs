33b740aa1b3eed09ce58873a43df0e5e8e80fcd4
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-29 13:33:51 +0300

    Update server.rs

--- frontend/src/server.rs
+++ frontend/src/server.rs
@@ -3,21 +3,25 @@ use warp::Filter;
 use warp::Rejection;
 use serde::Deserialize;
 
-use crate::service;
+use crate::service::get_address_info;
 
 #[derive(Debug, Deserialize)]
 pub struct FormData {
     address: String,
     start_block: String,
     end_block: String,
+    date: String, // Added to handle the new date filter
 }
 
+
 pub async fn form_handler(form_data: HashMap<String, String>) -> Result<impl warp::Reply, Rejection> {
+    println!("Received form data: {:?}", form_data);  // Debug print statement
     let address = form_data.get("address").cloned().unwrap_or_default();
     let start_block = form_data.get("start_block").cloned().unwrap_or_default();
     let end_block = form_data.get("end_block").cloned().unwrap_or_default();
+    let date = form_data.get("date").cloned().unwrap_or_default();  // Add this line
 
-    let response = service::get_address_info(&address, &start_block, &end_block).await.unwrap_or_default();
+    let response = get_address_info(&address, &start_block, &end_block, &date).await.unwrap_or_default();  // Update this line
 
     Ok(warp::reply::json(&response))
 }
