1cca41ce566b19a356ba9193162ee879661db0b3
qalassa <109701506+qalassa@users.noreply.github.com>
2023-06-29 13:35:33 +0300

    Update models.rs

--- common/src/models.rs
+++ common/src/models.rs
@@ -2,11 +2,54 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Transaction {
+    pub block_number: String,
+    pub time_stamp: String,
+    pub hash: String,
+    pub nonce: String,
+    pub block_hash: String,
+    pub transaction_index: String,
     pub from: String,
     pub to: String,
     pub value: String,
     pub gas: String,
     pub gas_price: String,
+    pub is_error: String,
+    pub txreceipt_status: String,
+    pub input: String,
+    pub contract_address: String,
+    pub cumulative_gas_used: String,
+    pub gas_used: String,
+    pub confirmations: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct TokenTransfer {
+    pub block_number: String,
+    pub time_stamp: String,
+    pub hash: String,
+    pub nonce: String,
+    pub block_hash: String,
+    pub from: String,
+    pub contract_address: String,
+    pub to: String,
+    pub value: String,
+    pub token_name: String,
+    pub token_symbol: String,
+    pub token_decimal: String,
+    pub transaction_index: String,
+    pub gas: String,
+    pub gas_price: String,
+    pub gas_used: String,
+    pub cumulative_gas_used: String,
+    pub input: String,
+    pub confirmations: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Balance {
+    pub status: String,
+    pub message: String,
+    pub result: String,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
